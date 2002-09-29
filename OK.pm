af21ec6678e3b78fbd238821007d6a2ba53cb28d
Jeffrey Hayes Anderson <jeffa@cpan.org>
2002-09-29 23:23:16 +0000

    Import of JEFFA/Tie-Scalar-OK-1.00 from CPAN.
    gitpan-cpan-distribution: Tie-Scalar-OK
    gitpan-cpan-version:      1.00
    gitpan-cpan-path:         JEFFA/Tie-Scalar-OK-1.00.tar.gz
    gitpan-cpan-author:       JEFFA
    gitpan-cpan-maturity:     released

---
+++ OK.pm
@@ -0,0 +1,92 @@
+package Tie::Scalar::OK;
+
+use strict;
+use base qw(Tie::Scalar);
+use vars qw($VERSION);
+
+$VERSION = '1.00';
+
+sub TIESCALAR {
+   my $class = shift;
+   my $self  = {
+      i  => 0,
+      ok => '',
+   };
+   return bless $self, $class;
+}
+
+sub STORE {
+   my ($self,$ok) = @_;
+   $self->{ok} = $ok ? 'ok' : 'not ok';
+   $self->{i}++;
+}
+
+sub FETCH {
+   my ($self) = @_;
+   return "$self->{ok} $self->{i}\n";
+}
+
+1;
+
+=pod
+
+=head1 NAME
+
+Tie::Scalar::OK - generate numbered 'ok/not ok' strings
+
+=head1 DESCRIPTION
+
+This module generates the output that test modules expect
+in a similar manner as Test.pm, i.e. "ok #" or "not ok #"
+strings are printed to STDOUT for pass and fail, respectively.
+
+=head1 SYNOPSIS
+
+   use strict;
+   use Tie::Scalar::OK;
+
+   my $ok;
+   tie $ok, 'Tie::Scalar::OK';
+
+   print $ok = (1 != 1);                        # not ok
+   print $ok = (2 == 1+1);                      # ok
+   print $ok = ('foo' eq join('',qw(f o o)));   # ok
+   print $ok = ('rab' ne scalar reverse 'bar'); # not ok
+   print $ok = ('phenobarbitone' =~ /bar/ );    # ok
+   print $ok = (0 != (32&(32-1)));              # not ok
+
+=head1 SEE ALSO
+
+Test
+Test::Harness
+Test::Simple
+Test::More
+
+=head1 AUTHOR 
+
+Jeffrey Hayes Anderson <captvanhalen@yahoo.com>
+
+=head1 COPYRIGHT
+
+Copyright (c) 2002 Jeffrey Hayes Anderson.
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify,
+merge, publish, distribute, sublicense, and/or sell copies of the
+Software, and to permit persons to whom the Software is furnished to
+do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+=cut
---
+++ test.pl
@@ -0,0 +1,26 @@
+# Before `make install' is performed this script should be runnable with
+# `make test'. After `make install' it should work as `perl test.pl'
+
+######################### We start with some black magic to print on failure.
+
+# Change 1..1 below to 1..last_test_to_print .
+# (It may become useful if the test is moved to ./t subdirectory.)
+
+BEGIN { $| = 1; print "1..3\n"; }
+END {print "not ok 1\n" unless $loaded;}
+use Tie::Scalar::OK;
+my $ok;
+tie $ok, 'Tie::Scalar::OK';
+$loaded = 1;
+print $ok = ($loaded == 1);
+
+
+print $ok = !(1 != 1);
+print $ok = (2 == 1+1);
+
+######################### End of black magic.
+
+# Insert your test code below (better if it prints "ok 13"
+# (correspondingly "not ok 13") depending on the success of chunk 13
+# of the test code):
+
